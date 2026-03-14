6a44abda804520e88d229d0bba141c767413bd9b
Aaron Wells <69355326+PhillipAWells@users.noreply.github.com>
2026-03-14 14:39:38 +0000

    fix(nestjs-shared): fix IPv6 regex to handle compressed notation

--- packages/nestjs-shared/src/common/services/error-sanitizer.service.ts
+++ packages/nestjs-shared/src/common/services/error-sanitizer.service.ts
@@ -63,7 +63,7 @@ export class ErrorSanitizerService implements LazyModuleRefService {
 	 * Covers various IPv6 formats: full form, compressed form, ::1, ::ffff:IPv4
 	 * Uses a simpler, non-backtracking pattern to avoid ReDoS (Regular Expression Denial of Service)
 	 */
-	private static readonly IPV6_REGEX = /([0-9a-fA-F]{1,4}:){1,7}[0-9a-fA-F]{1,4}|::1|::ffff:[0-9.]+|::/g;
+	private static readonly IPV6_REGEX = /(?:(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?::[0-9a-fA-F]{1,4}){1,6}|:(?::[0-9a-fA-F]{1,4}){1,7}|::(?:ffff(?::0{1,4})?:)?(?:25[0-5]|(?:2[0-4]|1?[0-9])?[0-9])(?:\.(?:25[0-5]|(?:2[0-4]|1?[0-9])?[0-9])){3}|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:25[0-5]|(?:2[0-4]|1?[0-9])?[0-9])(?:\.(?:25[0-5]|(?:2[0-4]|1?[0-9])?[0-9])){3})/gi;
 
 	/**
 	 * Default sensitive field names that are always redacted from context
